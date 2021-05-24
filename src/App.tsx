9ca625ceb3611a6b25230d651a56d6e7ef8f9964
Carlos Flores <carlos.flores@resuelve.mx>
2021-05-24 13:15:36 -0500

    Remove subfolder, all code is in root now

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende ~dinero~  React
+        </a>
       </header>
     </div>
   );
