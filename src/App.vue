1132ee1f83a5ecf381f345c63a52361a07fc89a9
Zero Dragon <carlos@floresbenavides.com>
2021-05-26 15:41:08 -0500

    Change all layouts to pug files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+#nav
+  router-link(to="/") Home
+  router-link(to="/about") About
+router-view
 </template>
 
 <style lang="stylus">
